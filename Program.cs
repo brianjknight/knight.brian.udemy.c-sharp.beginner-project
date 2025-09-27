e2d5a2604262ffb192424f1824c49b5cde22858d
Brian Knight <knight.j.brian@gmail.com>
2025-09-27 12:05:35 -0600

    working with variables

--- Program.cs
+++ Program.cs
@@ -14,6 +14,22 @@ namespace C_BeginnerProject
             Console.WriteLine("What is your favorite color?");
             Console.ReadLine();
             Console.WriteLine("Me too!");
+            Console.WriteLine();
+
+            // numbers
+            int i = 1;
+            long l = 1;
+            float f = 1.0f;
+            double d = 1.0;
+
+            // Strings and characters
+            char c = 'a';
+            string name = "Brian";
+
+            // variable type inference
+            var v = "var";
+            var var = 1;
+            
         }
     }
 }
