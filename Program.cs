e15187ebf3eed34a4657046123cd76ad0d7b1e32
Brian Knight <knight.j.brian@gmail.com>
2025-09-28 15:16:00 -0600

    loops and arrays

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace C_BeginnerProject
 {
@@ -16,24 +17,24 @@ namespace C_BeginnerProject
             //Console.WriteLine("Me too!");
             //Console.WriteLine();
 
-            // numbers
+            // ----- numbers -----
             int i = 1;
             long l = 1;
             float f = 1.0f;
             double d = 1.0;
 
-            // Strings and characters
+            // ----- Strings and characters -----
             char c = 'a';
             string name = "Brian";
 
-            // variable type inference
+            // ----- variable type inference -----
             var v = "var";
             var var = 1;
 
-            // Boolean
+            // ----- Boolean -----
             bool isBool = true;
 
-            // Arithmetic operators
+            // ----- Arithmetic operators -----
             int one = 1;
             int two = 2;
             int three = 3;
@@ -45,7 +46,9 @@ namespace C_BeginnerProject
 
             Console.WriteLine(sum + " | " + product + " | " + quotient + " | " + mod + " | ");
 
-            int age = 100;
+
+            // ----- Conditional statements -----
+            int age = 18;
 
             if (age >= 18)
             {
@@ -60,7 +63,7 @@ namespace C_BeginnerProject
                 Console.WriteLine("minor");
             }
 
-            switch (age) 
+            switch (age)
             {
                 case 1:
                 case 2: // C# does not allow multiple case values on same line
@@ -74,6 +77,49 @@ namespace C_BeginnerProject
                     Console.WriteLine("other");
                     break;
             }
+
+
+            // ----- Loops -----
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.WriteLine(j);
+            }
+
+            int x = 100;
+            while (x <= 105)
+            {
+                Console.WriteLine(x++);
+            }
+
+            do
+            {
+                Console.WriteLine(x++);
+            } while (x <= 110);
+            Console.WriteLine();
+
+            string greet = GreetUser("Brian");
+            Console.WriteLine(greet);
+            Console.WriteLine();
+
+
+            // ----- Arrays -----
+            int[] odds = new int[5];
+            odds[0] = 1;
+            odds[1] = 3;
+            odds[2] = 5;
+            odds[3] = 7;
+            odds[4] = 9;
+
+            int[] evens = {2,4,6,8,10};
+
+            Console.WriteLine(evens[0]);
+        }
+
+        // ----- Methods -----
+        static string GreetUser(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+            return "You are learning C#!";
         }
     }
 }
